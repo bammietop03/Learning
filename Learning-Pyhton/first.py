fbc59d43f7dd64d9b4dad01a7dc4b877b3e57b11
Bamiwo Adebayo <bammietop@gmail.com>
2023-07-16 17:49:44 +0100

    Update Readme.md

---
+++ Learning-Pyhton/first.py
@@ -0,0 +1,7 @@
+name = "bamiwo adebayo"
+
+first_name = name[:7].upper() # bamiwo will be changed to uppercase
+last_name = name[7:].lower() # adebayo will be changed to lowercase
+
+print(first_name) # output - BAMIWO
+print(last_name) # output - adebayo
